798f01990917c3a4617f34f0777b08709dd74432
Nanda Datta Buri <burinandadatta@gmail.com>
2026-04-30 16:49:29 +0530

    Fixed SignUpPage import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import ChatPage from './pages/ChatPage'
 import LoginPage from './pages/LoginPage'
-import SignUpPage from './pages/SignupPage'
+import SignUpPage from './pages/SignUpPage'
 import { useAuthStore } from './store/useAuthStore'
 import PageLoader from './components/PageLoader'
 import { Toaster } from 'react-hot-toast';
